0671ceb5575311ae9978efbd4a126392570a299f
ameer-hamza-brainx <ameer.hamza@brainxtech.com>
2023-10-05 11:40:20 +0500

    requested changes

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const User = require("../models/user");
 
 
 app.use(session({
-  secret: 'secret_key_here',
-  resave: false,
-  saveUninitialized: true
+    secret: 'secret_key_here',
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use(express.json());
@@ -23,256 +23,255 @@ function generateToken(length) {
         token += characters.charAt(randomIndex);
     }
     return token;
-  }
+}
 
-function isLogIn(req,res,next){
-    if(req.session.loggedIn)
-    {
+function isLogIn(req, res, next) {
+    if (req.session.loggedIn) {
         next();
     }
-    else{
+    else {
         res.send("Log In first");
     }
 }
 
-async function isVerified(req,res,next){
-    const foundUser = await User.findOne({email:req.session.email});
+async function getUser(req, res, next) {
+    try {
+        req.user = await User.findOne({ email: req.session.email });
+    }
+    catch (e) {
+        res.status(500).send("Internal error");
+    }
+    next();
+}
+async function isVerified(req, res, next) {
+    try {
+        req.user = await User.findOne({ email: req.session.email });
+    }
+    catch (e) {
+        res.status(500).send("Internal error");
+    }
 
-    if(foundUser.isVerified)
-    {
+    if (req.user.isVerified) {
         next();
     }
-    else{
+    else {
         res.send("Verify your email first");
     }
 }
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).send("<h1>Home page</h1>");
 });
 
 //Signup api
 
-app.post("/signup",async(req,res)=>{
+app.post("/signup", async (req, res) => {
+    try {
+        const foundUser = await User.findOne({ email: req.body.email });
+        if (!foundUser) {
+            const newUser = new User(req.body);
+            newUser.save().then(() => {
+                res.send("Sucessfully registered")
+            })
+                .catch((e) => {
+                    console.log(e);
+                    res.status(400).send("Some problem occured")
+                });
+        }
+        else {
+            res.send("User already exist");
+        }
+    }
+    catch (e) {
+        res.status(500).send("Internal error");
+    }
 
-   const foundUser =await User.findOne({email:req.body.email});
-   if(!foundUser)
-   {
-       const newUser = new User(req.body);
-        newUser.save().then(()=>{
-        res.send("Sucessfully registered")})
-        .catch((e)=>{
-            console.log(e);
-            res.status(400).send("Some problem occured")});
-   }
-   else{
-    res.send("User already exist");
-   }
-    
+
 });
 
 // Login Api
 
-app.post("/signin",async(req,res)=>{
-    const foundUser =await User.findOne({email:req.body.email});
-    if(foundUser)
-    {
-        if(foundUser.password == req.body.password)
-        {
-            req.session.loggedIn = true;
-            req.session.email = req.body.email;
-            res.status(200).send("Signin successful");
+app.post("/signin", async (req, res) => {
+    try {
+        const foundUser = await User.findOne({ email: req.body.email });
+        if (foundUser) {
+            if (foundUser.password == req.body.password) {
+                req.session.loggedIn = true;
+                req.session.email = req.body.email;
+                res.status(200).send("Signin successful");
+            }
+            else {
+                res.status(401).send("Invalid password");
+            }
         }
-        else
-        {
-            res.status(401).send("Invalid password");
+        else {
+            res.status(401).send("Invalid credentials");
         }
     }
-    else
-    {
-        res.status(401).send("Invalid credentials");
+    catch (e) {
+        res.status(500).send("Internal error");
     }
+
 });
 
 // change password
 
-app.post("/changepassword",isLogIn,isVerified,async(req,res)=>{
-    let user = await User.findOne({email:req.session.email});
-    user.password = req.body.password;
-    try{
-        user.save();
+app.post("/changepassword", isLogIn, isVerified, getUser, async (req, res) => {
+    req.user.password = req.body.password;
+    try {
+        req.user.save();
         res.send("password changed");
     }
-    catch(e){
+    catch (e) {
         res.status(500).send("problem occured");
     }
 });
 
-app.get("/verifyEmail",isLogIn,async(req,res)=>{
-    let user = await User.findOne({email:req.session.email});
-    user.isVerified = true;
-    user.save().then(()=>{
-        res.send("Email verified")})
-        .catch((e)=>{
+app.get("/verifyEmail", isLogIn, getUser, async (req, res) => {
+    req.user.isVerified = true;
+    req.user.save().then(() => {
+        res.send("Email verified")
+    })
+        .catch((e) => {
             console.log(e);
-            res.status(500).send("problem occured")});
+            res.status(500).send("problem occured")
+        });
 });
 
 
 
 // Task create API
-app.post("/createTask",isLogIn,isVerified, async(req,res)=>{
-    let user = await User.findOne({email:req.session.email});
-            
+app.post("/createTask", isLogIn, isVerified, getUser, async (req, res) => {
+
     let newObj = {
-        "task":req.body.task
-        }
-    user.tasks.push(newObj);
-    user.save().then(()=>{
-    res.send("task added");
-    }).catch((e)=>{
+        "task": req.body.task
+    }
+    req.user.tasks.push(newObj);
+    req.user.save().then(() => {
+        res.send("task added");
+    }).catch((e) => {
         res.status(400).send("Internal error");
     })
 });
 
 //Task remove API
 
-app.post("/removeTask",isLogIn,isVerified,async(req,res)=>{
-    let user = await User.findOne({email:req.session.email});
+app.post("/removeTask", isLogIn, isVerified, getUser, async (req, res) => {
     let taskExist = false;
-    if(user.tasks)
-    {
-        let tasks = user.tasks;
-        tasks.forEach((task,index) => {
-            if(task._id == req.body.id)
-            {
+    if (req.user.tasks) {
+        let tasks = req.user.tasks;
+        tasks.forEach((task, index) => {
+            if (task._id == req.body.id) {
                 taskExist = true;
                 tasks.splice(index, 1);
-                user.tasks = tasks;
+                req.user.tasks = tasks;
             }
         });
-        if(taskExist)
-        {
-            user.save().then(()=>{
+        if (taskExist) {
+            req.user.save().then(() => {
                 res.status(200).send("Task removed");
-            }).catch((e)=>{
+            }).catch((e) => {
                 res.status(500).send("Internal error");
             })
         }
-        else
-        {
+        else {
             res.status(200).send("Task not exist");
         }
     }
-    else
-    {
+    else {
         res.send("Task not exist");
     }
 });
 //Task update API
 
-app.patch("/updateTask",isLogIn,isVerified,async(req,res)=>{
-    let user = await User.findOne({email:req.session.email});
+app.patch("/updateTask", isLogIn, isVerified, getUser, async (req, res) => {
     let taskExist = false;
-    if(user.tasks)
-    {
-        let tasks = user.tasks;
-        tasks.forEach((task,index) => {
-            if(task._id == req.body.id)
-            {
+    if (req.user.tasks) {
+        let tasks = req.user.tasks;
+        tasks.forEach((task, index) => {
+            if (task._id == req.body.id) {
                 taskExist = true;
                 task.task = req.body.updatedTask;
             }
         });
-        if(taskExist)
-        {
-            user.save().then(()=>{
+        if (taskExist) {
+            req.user.save().then(() => {
                 res.status(200).send("Task Updated");
-            }).catch((e)=>{
+            }).catch((e) => {
                 res.status(500).send("Internal error");
             })
         }
-        else
-        {
+        else {
             res.status(200).send("Task not exist");
         }
     }
-    else
-    {
+    else {
         res.send("Task not exist");
     }
 });
 
 // Get tasks
 
-app.get("/getTasks",isLogIn,isVerified,async(req,res)=>{
-    let user = await User.findOne({email:req.session.email});
-    if(user.tasks)
-    {
-        res.send(user.tasks);
+app.get("/getTasks", isLogIn, isVerified, getUser, async (req, res) => {
+    if (req.user.tasks) {
+        res.send(req.user.tasks);
     }
-    else
-    {
+    else {
         res.send("Task not exist");
     }
-    
+
 });
 
 
-app.post("/forgotPassword",async(req,res)=>{
-    const foundUser = await User.findOne({email:req.body.email});
+app.post("/forgotPassword", async (req, res) => {
+    const foundUser = await User.findOne({ email: req.body.email });
     const token = generateToken(16);
-    if(foundUser)
-    {
+    if (foundUser) {
         const expirationTime = new Date();
         expirationTime.setMinutes(expirationTime.getMinutes() + 2);
         foundUser.token = {
-            "key":token,
-            "expiry":expirationTime
+            "key": token,
+            "expiry": expirationTime
         }
-        foundUser.save().then(()=>{
-            res.send("http://localhost:5000/resetPassword/"+token)
-        }).catch((e)=>{
+        foundUser.save().then(() => {
+            res.send("http://localhost:5000/resetPassword/" + token)
+        }).catch((e) => {
             console.log(e);
-            res.status(500).send("Some problem occured")});
+            res.status(500).send("Some problem occured")
+        });
     }
-    else
-    {
+    else {
         res.send("user not exist");
     }
 });
 
 //Reset password
 
-app.post("/resetPassword/:token",async(req,res)=>{
-    const foundUser = await User.findOne({"token.key":req.params.token});
-    if(foundUser)
-    {
+app.post("/resetPassword/:token", async (req, res) => {
+    const foundUser = await User.findOne({ "token.key": req.params.token });
+    if (foundUser) {
         const dateNow = new Date();
         const expiryDate = new Date(foundUser.token.expiry);
-        if(dateNow <= expiryDate)
-        {
+        if (dateNow <= expiryDate) {
             foundUser.password = req.body.password;
-            foundUser.save().then(()=>{
+            foundUser.save().then(() => {
                 res.send("pasword changed");
-            }).catch((e)=>{
+            }).catch((e) => {
                 res.status(500).send(e);
             })
         }
-        else
-        {
+        else {
             res.status(400).send("token expired");
         }
     }
-    else
-    {
+    else {
         res.send("Invalid token");
     }
-    
+
 });
 
 // listening on port
 
-app.listen(port,()=>{
-    console.log("listening on port "+port);
+app.listen(port, () => {
+    console.log("listening on port " + port);
 })
